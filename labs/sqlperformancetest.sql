8466fad2c1decb0da30a5674f49a5ca3319fb15a
Albert Lee <albert_lee@163.com>
2019-03-29 17:09:23 +0800

    modify: insert 2 replace

--- labs/sqlperformancetest.sql
+++ labs/sqlperformancetest.sql
@@ -1,17 +1,17 @@
 delete from section
 where year>=2018;
 
-insert into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+replace into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
 select course_id,sec_id,'Spring',2019,building,room_number,time_slot_id 
 from section as s
 where  s.year<2018 and s.course_id>=300;
 
-insert into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+replace into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
 select course_id,sec_id,'Fall',2018,building,room_number,time_slot_id 
 from section as s
 where  s.year<2018 and s.course_id>=300;
 
-insert into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+replace into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
 select course_id,sec_id,semester,2017,building,room_number,time_slot_id 
 from section as s
 where  s.year<2018;
