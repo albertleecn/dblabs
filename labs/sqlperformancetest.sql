15fcb2208e97b1c22e0ce8e45e5340314538c332
Albert Lee <albert_lee@163.com>
2019-03-29 14:23:40 +0800

    add sqlperformancetest.sql

---
+++ labs/sqlperformancetest.sql
@@ -0,0 +1,11 @@
+insert into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+select course_id,sec_id,'Spring',2019,building,room_number,time_slot_id 
+from section as s
+where  s.year<2018 and s.course_id>=300
+
+insert into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+select course_id,sec_id,'Fall',2018,building,room_number,time_slot_id 
+from section as s
+where  s.year<2018 and s.course_id>=300
+
+commit
