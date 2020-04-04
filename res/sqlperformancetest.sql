e82157cc0a2e9d4a6087353b5ffb70daa6961d11
albertlee <albert_lee@163.com>
2020-04-04 17:27:24 +0800

    add sqlperformancetest.sql

---
+++ res/sqlperformancetest.sql
@@ -0,0 +1,19 @@
+delete from section
+where year>=2018;
+
+replace into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+select course_id,sec_id,'Spring',2019,building,room_number,time_slot_id 
+from section as s
+where  s.year<2018 and s.course_id>=300;
+
+replace into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+select course_id,sec_id,'Fall',2018,building,room_number,time_slot_id 
+from section as s
+where  s.year<2018 and s.course_id>=300;
+
+replace into section(course_id,sec_id,semester,year,building,room_number,time_slot_id)
+select course_id,sec_id,semester,2017,building,room_number,time_slot_id 
+from section as s
+where  s.year<2018;
+
+commit;
